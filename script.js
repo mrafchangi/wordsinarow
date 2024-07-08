815cdec5f891e546a9d9c9a47720df2927d98951
mrafchangi <itsmrafchangy@gmail.com>
2024-07-09 01:31:45 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const gameContainer = document.getElementById('game-container');
+    gameContainer.innerHTML = '<p>Game content goes here</p>';
+});
